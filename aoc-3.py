e1201a8912ce3ffde9fe50f50c3417209c65efd3
rodgerswisdom <130440506+rodgerswisdom@users.noreply.github.com>
2024-12-04 11:44:06 +0300

    Feat: Solve sum of products

--- aoc-3.py
+++ aoc-3.py
@@ -9,6 +9,8 @@
 """
     search the list and look for only what we need - regex 
     then add them
+
+    the matches come as a list of string.
 """
 import re
 
@@ -21,22 +23,37 @@ def aoc3(file):
             input_string = content
 
             pattern =  r'mul\(\d+,\d+\)'
+            # pattern = r'\d+'
 
             matches = re.findall(pattern, input_string)
-            print(f"{len(matches)}")
+            return matches
 
     except Exception as e:
         print(f"{e}")
+        return -1
     finally:
         if file in locals():
             file.close()
             print(f"{file} has been closed")
 
+def calc_mul(matches):
+    sum_of_products = 0
+
+    for match in matches:
+        numbers = re.findall(r'\d+', match)
+#        print(numbers)
+
+        if len(numbers) == 2:
+            a, b = map(int, numbers)
+        sum_of_products += a * b
+    return sum_of_products
 
 def main():
     file = '3i.txt'
 
-    aoc3(file)
+    matches = aoc3(file)
+    sln = calc_mul(matches)
+    print(sln)
 
 if __name__ == "__main__":
     main()
